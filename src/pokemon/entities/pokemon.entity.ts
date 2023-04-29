b56fb0838de43df887523926744f26384f800311
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-04-29 16:34:26 -0300

    create pokemon schema and connect with pokemon module

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,18 @@
-export class Pokemon {}
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose/dist';
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
